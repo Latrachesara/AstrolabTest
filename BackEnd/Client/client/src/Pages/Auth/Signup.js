3ced0182259828dc4b42d81068cfea0ac5ef41b7
latrachesara <ltchsara@gmail.com>
2021-08-02 21:44:17 +0100

    react

---
+++ BackEnd/Client/client/src/Pages/Auth/Signup.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './../../Style/SignUp.css';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
+function Signup() {
+    const classes = useStyles();
+    
+
+    return (
+        <div  className="body">
+            
+            <form className={classes.root} noValidate autoComplete="off">
+      <TextField id="standard-basic" label="Standard" />
+      <TextField id="filled-basic" label="Filled" variant="filled" />
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+    </form>
+         
+        </div>
+    )
+}
+
+export default Signup
